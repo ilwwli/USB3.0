dc578340d39719c5ccc3d72143749e64d13b1446
ZXH <863886245@qq.com>
2017-10-30 11:20:53 +0800

    x64

--- USB3.0.cpp
+++ USB3.0.cpp
@@ -3,17 +3,58 @@
 
 #include "stdafx.h"
 #include "FTD3XX.h" 
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+#include <ctime>
 #pragma comment(lib, "FTD3XX.lib")
+
+#define BUFFER_SIZE 0x10000000
+
+FT_STATUS Initialization(FT_HANDLE &);
+FT_STATUS WINAPI ReadThread(LPVOID);
+typedef struct FT_STRUCT
+{
+	FT_HANDLE ftHandle;
+	FT_STATUS ftStatus;
+} *pFT_STRUCT;
 
 int main()
 {
-    return 0;
+	FT_STATUS ftStatus;
+	FT_HANDLE ftHandle;
+	FT_STRUCT ftStruct;
+	ftStatus = Initialization(ftHandle);
+	if (FT_FAILED(ftStatus))
+	{
+		printf("Initialization Failed\n");
+		system("pause");
+		return 0;
+	}	
+	HANDLE hThread;
+	hThread = CreateThread(
+		NULL,                   // default security attributes
+		0,                      // use default stack size  
+		ReadThread,				// thread function name
+		&ftStruct,				// argument to thread function 
+		0,                      // use default creation flags 
+		NULL);					// returns the thread identifier 								
+
+	if (hThread == NULL)
+	{
+		printf("Create Thread Failed\n");
+		ExitProcess(3);
+	}
+	//WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
+	WaitForSingleObject(hThread, INFINITE);
+	// End of main thread creation loop.
+	system("pause");
+	return 0;
 }
 
-FT_HANDLE Initialization()
+FT_STATUS Initialization(FT_HANDLE &ftHandle)
 {
-	FT_STATUS ftStatus; 
-	FT_HANDLE ftHandle;
+	FT_STATUS ftStatus; 	
 	DWORD numDevs = 0;
 	ftStatus = FT_CreateDeviceInfoList(&numDevs); 
 	if (!FT_FAILED(ftStatus) && numDevs > 0) 
@@ -41,9 +82,7 @@ FT_HANDLE Initialization()
 		else
 			printf("Get Device Info List Failed\n");		 
 		ftStatus = FT_Create(devInfo[0].SerialNumber, FT_OPEN_BY_SERIAL_NUMBER, &ftHandle);
-		if (!FT_FAILED(ftStatus))
-		{	}
-		else
+		if (FT_FAILED(ftStatus))
 		{
 			printf("Create Failed\n");
 			FT_Close(ftHandle);
@@ -52,6 +91,53 @@ FT_HANDLE Initialization()
 	}
 	else
 		printf("Create Device Info List Failed\n");
-	return ftHandle;
+	return ftStatus;
 }
 
+FT_STATUS WINAPI ReadThread(LPVOID lpParam)
+{
+	FT_STATUS ftStatus;
+	FT_HANDLE ftHandle;
+	pFT_STRUCT pftStruct = (pFT_STRUCT)lpParam;
+	ftHandle = pftStruct->ftHandle;
+	UCHAR acBuf[BUFFER_SIZE] = { 0xFF };
+	ULONG ulBytesTransferred = 0;
+	clock_t timeLapse;
+	TCHAR msgBuf[255];
+
+	HANDLE hStdout;
+	// Make sure there is a console to receive output results. 
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	if (hStdout == INVALID_HANDLE_VALUE)
+	{
+		ftStatus =  FT_OTHER_ERROR;
+		pftStruct->ftStatus = ftStatus;
+		return ftStatus;
+	}
+	
+	
+	while (1)
+	{
+		//ULONG Counter = BUFFER_SIZE;
+		timeLapse = clock();
+		ftStatus = FT_WritePipe(ftHandle, 0x02, acBuf, BUFFER_SIZE, &ulBytesTransferred, NULL);
+		if (FT_FAILED(ftStatus))
+		{
+			ftStatus = FT_AbortPipe(ftHandle, 0x02);
+			if (FT_FAILED(ftStatus))
+			{
+				pftStruct->ftStatus = ftStatus;
+				return ftStatus;
+			}				
+		}
+		timeLapse = clock() - timeLapse;
+		// Print the result using thread-safe functions.
+		size_t cchStringSize;
+		DWORD dwChars;
+		StringCchPrintf(msgBuf, 255, TEXT("%dsec Elapsed for Transmitting %dMB Data\n"),
+			((double)timeLapse) / CLOCKS_PER_SEC, ulBytesTransferred);
+		StringCchLength(msgBuf, 255, &cchStringSize);
+		WriteConsole(hStdout, msgBuf, (DWORD)cchStringSize, &dwChars, NULL);
+	}
+	return 0;
+}
